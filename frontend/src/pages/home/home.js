aca40dc3bc547fe393b165ef52923d51905cba9f
Eric Schnelker <eps1923@utulsa.edu>
2024-02-23 22:19:36 -0600

    fixed polling issues with docker

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -6,7 +6,7 @@ const Home = () => {
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"></link>
             <div class="jumbotron jumbotron-fluid position-relative start-50">
                 <div class="container position-relative start-50">
-                    <p class="display-4">Welcome to CssssaaaaaaaaaaaaaaaaaaaaaaaaaaaasAVSSS, the Court Attendance Verification Software</p>
+                    <p class="display-4">Welcome to CAVS, the Court Attendance Verification Software</p>
                 </div>
             </div>
         </div>
