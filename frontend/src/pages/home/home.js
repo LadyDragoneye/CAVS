0410d7671a459e098543b83d6740923dd9573c7e
Eric Schnelker <eps1923@utulsa.edu>
2024-03-05 18:30:14 -0600

    Update home.js

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -6,7 +6,7 @@ const Home = () => {
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"></link>
             <div style={{ backgroundColor : "transparent"}} class="jumbotron jumbotron-fluid position-relative start-50">
                 <div class="container position-relative start-50">
-                    <p class="display-4">Welcome to this CAVS, the Court Attendance Verification Software</p>
+                    <p class="display-1 text-center">Welcome to this CAVS, the Court Attendance Verification Software</p>
                 </div>
             </div>
         </div>
