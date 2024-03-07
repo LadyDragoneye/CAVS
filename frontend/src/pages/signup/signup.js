3ea9748dacbb95f37aba35ea2b76315b57a33b33
brooksrog8 <114731707+brooksrog8@users.noreply.github.com>
2024-03-07 11:06:27 -0600

    Update signup.js

--- frontend/src/pages/signup/signup.js
+++ frontend/src/pages/signup/signup.js
@@ -35,7 +35,6 @@ function Registerpage() {
                   <div className="row g-0">
                     <div className="col-md-6 col-lg-5 d-none d-md-block">
                       <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
                         alt="login form"
                         className="img-fluid"
                         style={{ borderRadius: "1rem 0 0 1rem" }}
