e7e10d28fad41d705e50f25fb15563c009238593
brooksrog8 <bcr9868@utulsa.edu>
2024-03-06 21:58:10 -0600

    export auth context error

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,24 +1,71 @@
-from rest_framework.views import APIView
+# from rest_framework.views import APIView
 
-# from app.serializer import ReactSerializer
-from . models import *
-from . serializer import *
+# # from app.serializer import ReactSerializer
+# from . models import *
+# from . serializer import *
+# from rest_framework.response import Response
+# # Create your views here.
+
+
+# class ReactView(APIView):
+
+#     serializer_class = ReactSerializer
+
+#     def get(self, request):
+#         output = [{"employee": output.employee, "department": output.department}
+#                   for output in React.objects.all()] # Get all instances of the React model and construct output data
+#         return Response(output) # Return a response containing the output data
+
+#     def post(self, request):
+
+#         serializer = ReactSerializer(data=request.data) # Create a serializer instance with request data
+#         if serializer.is_valid(raise_exception=True): # Check if the data passed to the serializer is valid
+#             serializer.save() # Save the data to the database
+#             return Response(serializer.data) # Return a response with serialized data
+from django.shortcuts import render
+from django.http import JsonResponse
+from app.models import User
+
+from app.serializer import MyTokenObtainPairSerializer, RegisterSerializer
+
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
-# Create your views here.
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
 
 
-class ReactView(APIView):
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
-    serializer_class = ReactSerializer
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
 
-    def get(self, request):
-        output = [{"employee": output.employee, "department": output.department}
-                  for output in React.objects.all()] # Get all instances of the React model and construct output data
-        return Response(output) # Return a response containing the output data
 
-    def post(self, request):
+# Get All Routes
 
-        serializer = ReactSerializer(data=request.data) # Create a serializer instance with request data
-        if serializer.is_valid(raise_exception=True): # Check if the data passed to the serializer is valid
-            serializer.save() # Save the data to the database
-            return Response(serializer.data) # Return a response with serialized data
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        '/api/token/',
+        '/api/register/',
+        '/api/token/refresh/'
+    ]
+    return Response(routes)
+
+
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def testEndPoint(request):
+    if request.method == 'GET':
+        data = f"Congratulation {request.user}, your API just responded to GET request"
+        return Response({'response': data}, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        text = "Hello buddy"
+        data = f'Congratulation your API just responded to POST request with text: {text}'
+        return Response({'response': data}, status=status.HTTP_200_OK)
+    return Response({}, status.HTTP_400_BAD_REQUEST)
--- backend/djangoproj/settings.py
+++ backend/djangoproj/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,11 +26,17 @@ SECRET_KEY = 'django-insecure-2k1v-ir_9un@-_sh&p=lnocm*-jdlb3mp6z7h7z035p#t2vqp$
 DEBUG = True
 
 ALLOWED_HOSTS = []
+# settings.py
+
+# settings.py
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app',
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
 ]
 
@@ -54,8 +61,8 @@ MIDDLEWARE = [
 ]
 
 
-REST_FRAMEWORK = {"DEFAULT_PERMISSION_CLASSES": [
-    "rest_framework.permissions.AllowAny"]}
+
+
 
 CORS_ORIGIN_ALLOW_ALL = True
 
@@ -109,7 +116,43 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=50),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -133,3 +176,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'app.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
