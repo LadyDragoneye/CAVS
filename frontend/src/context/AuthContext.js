634144becf0a92f7c0e559c5a322566e839aadf7
brooksrog8 <bcr9868@utulsa.edu>
2024-03-16 20:07:01 -0500

    implemented user signup

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -18,6 +18,7 @@ export const AuthProvider = ({children}) => {
 
 
 
+    
     let loginUser = async (e )=> {
         e.preventDefault()
         console.log('Form submitted')
@@ -41,6 +42,45 @@ export const AuthProvider = ({children}) => {
         }
 
     }
+
+    // signupUser
+    let signupUser = async (e) => {
+        e.preventDefault();
+        console.log('Sign-up form submitted');
+    
+        // Fetch API call to sign up the user
+        let response = await fetch('http://localhost:8001/app/register/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email:e.target.email.value,
+                username:e.target.username.value,
+                password:e.target.password.value,
+                password2: e.target.password2.value, // Update field name here
+            })
+            
+        });
+        console.log('posted');
+
+        let data = await response.json();
+    
+        if (response.status === 201) { // Assuming status code 201 for successful signup
+            console.log('User signed up successfully:', data);
+    
+            // You may handle success cases like redirecting to login page or any other actions
+            // For now, let's just log the response and display an alert
+            alert('User signed up successfully!');
+            history('/login');
+        } else {
+            console.error('Error signing up user:', data);
+            alert('Something went wrong during sign-up!');
+        }
+    }
+
+
+
     let logoutUser = () => {
         setAuthTokens(null)
         setUser(null)
@@ -78,6 +118,7 @@ export const AuthProvider = ({children}) => {
         user:user,
         authTokens:authTokens,
         loginUser:loginUser,
+        signupUser:signupUser,
         // logoutUser:logoutUser, // need to add logout button
     }
 
