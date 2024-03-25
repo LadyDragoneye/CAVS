f24083950e46ce2d483e2bad2b092b4a44026727
brooksrog8 <bcr9868@utulsa.edu>
2024-03-25 16:53:46 -0500

    linking db to app

--- backend/djangoproj/settings.py
+++ backend/djangoproj/settings.py
@@ -97,8 +97,12 @@ WSGI_APPLICATION = 'djangoproj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'USERS',
+        'USER':'admin',
+        'PASSWORD':'PASSWORD',
+        'HOST':'cavs-database.czog2aoew1bg.us-east-1.rds.amazonaws.com',
+        'PORT':'3306'
     }
 }
 
