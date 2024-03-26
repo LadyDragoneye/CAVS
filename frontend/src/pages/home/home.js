2a9f0a386e17220eb46500f5bf3a461c23de63d7
Eric Schnelker <eps1923@utulsa.edu>
2024-03-26 12:01:35 -0500

    Update home.js

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -17,29 +17,10 @@ const Home = () => {
             </div>
 
 
-            <div class="container">
-                <div class ="row">
-                    <div class="col-md-4">
-                        <h2>Heading</h2>
-                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="container">
-                <div class ="row">
-                    <div class="col-md-4">
-                        <h2>Heading</h2>
-                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="container">
-                <div class ="row">
-                    <div class="col-md-4">
-                        <h2>Heading</h2>
-                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
-                    </div>
-                </div>
+            <div class="jumbotron">
+                <h2 class='font-weight-bold'>About CAVS</h2>
+                <h4 class='text-center'>The purpose of the Court Attendance Verification Software (CAVS) is to allow legal professionals and law enforcement to better coordinate and help ensure compliance.</h4>
+                <h4 class='text-center'>The website's main objective is to allow court dates to be added to a calendar, viewable by law enforcement and legal professionals. From there, law enforcement confirms their attendance at said court date.</h4>
             </div>
             <hr/>
 
