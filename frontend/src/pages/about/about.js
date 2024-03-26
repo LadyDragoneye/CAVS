798eeb55785a210cb2985849b73bca8e40eb9766
Eric Schnelker <eps1923@utulsa.edu>
2024-03-26 11:40:32 -0500

    About Changes

--- frontend/src/pages/about/about.js
+++ frontend/src/pages/about/about.js
@@ -7,13 +7,9 @@ const About = () => {
     return (
         <div>
             <div class="jumbotron">
-                <h1 class="display-4 text-center">Hello, world!</h1>
-                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-                <hr class="my-4"></hr>
-                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                <p class="lead">
-                    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-                </p>
+                <h1>About CAVS</h1>
+                <p class="lead">The purpose of the Court Attendance Verification Software (CAVS) is to allow legal professionals and law enforcement to better coordinate and help ensure compliance.</p>
+                <p class="lead">The website's main objective is to allow court dates to be added to a calendar, viewable by law enforcement and legal professionals. From there, law enforcement confirms their attendance at said court date.</p>
             </div>
         </div>
     );
