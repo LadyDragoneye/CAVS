c0f4572e808c7df1635c1b9564e95c67bf1db2ca
Eric Schnelker <eps1923@utulsa.edu>
2024-05-02 17:51:59 -0500

    update home.js
    fixed formatting

--- frontend/src/pages/home/home.js
+++ frontend/src/pages/home/home.js
@@ -21,32 +21,25 @@ const Home = () => {
             </div>
 
 
-            <div class="container border border-primary">
+            <div class="container">
                 <div class ="row">
-                    <div class="col-md-4">
-                        <h2>About CAVS</h2>
-                    <p>The purpose of the Court Attendance Verification Software (CAVS) is to allow legal professionals and law enforcement to better coordinate and help ensure compliance.
+                    <div class="container">
+                        <div class="col-md-4">
+                            <h2>About CAVS</h2>
+                        <p>The purpose of the Court Attendance Verification Software (CAVS) is to allow legal professionals and law enforcement to better coordinate and help ensure compliance.
 The website's main objective is to allow court dates to be added to a calendar, viewable by law enforcement and legal professionals. From there, law enforcement confirms their attendance at said court date.</p>
+                        </div>
+                        <div class="col-md-4">
+                            <h2>Heading</h2>
+                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+                        </div>
+                        <div class="col-md-4">
+                            <h2>Heading</h2>
+                        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="container">
-                <div class ="row">
-                    <div class="col-md-4">
-                        <h2>Heading</h2>
-                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="container">
-                <div class ="row">
-                    <div class="col-md-4">
-                        <h2>Heading</h2>
-                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
-                    </div>
-                </div>
-            </div>
-            <hr/>
 
         </div>
     );
