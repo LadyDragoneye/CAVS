cf0bd8e38c4f0e36d9d01acaa4bcfff860e667f6
Eric Schnelker <eps1923@utulsa.edu>
2024-05-02 16:45:42 -0500

    fixed css
    I am god

--- frontend/src/pages/account/account.js
+++ frontend/src/pages/account/account.js
@@ -1,37 +1,38 @@
 // Filename - pages/account.js
 import React from "react";
 import "./account.css";
-
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"></link>
 
 const Account = () => {
     return (
-        <div className="container rounded bg-white mt-5 mb-5">
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"></link>
-            <link rel="stylesheet" type="text/css" href="./account.css"/>
-
-            <div className="row justify-content-center"> 
-                <div className="col-md-9 border-right">
-                    <div className="p-3 py-5">
-                        <div className="d-flex justify-content-between align-items-center mb-3">
-                            <h4 className="text-right">Profile</h4>
+    <html class="html-info">
+    <body class="body-info">
+        <div class="container rounded bg-white mt-5 mb-5">
+            <div class="row justify-content-center"> 
+                <div class="col-md-9 border-right">
+                    <div class="p-3 py-5">
+                        <div class="d-flex justify-content-between align-items-center mb-3">
+                            <h4 class="text-right">Profile</h4>
                         </div>
-                        <div className="row mt-2">
-                            <div className="col-md-6"><label className="labels">Name</label></div>
+                        <div class="row mt-2">
+                            <div class="col-md-6"><label class="labels">Name</label></div>
                         </div>
-                        <div className="row mt-3">
-                            <div className="col-md-12"><label className="labels">Mobile Number</label></div>
-                            <div className="col-md-12"><label className="labels">Address Line 1</label></div>
-                            <div className="col-md-12"><label className="labels">Address Line 2</label></div>
-                            <div className="col-md-12"><label className="labels">Postcode</label></div>
-                            <div className="col-md-12"><label className="labels">State</label></div>
-                            <div className="col-md-12"><label className="labels">Area</label></div>
-                            <div className="col-md-12"><label className="labels">Email ID</label></div>
-                            <div className="col-md-12"><label className="labels">Education</label></div>
+                        <div class="row mt-3">
+                            <div class="col-md-12"><label class="labels">Mobile Number</label></div>
+                            <div class="col-md-12"><label class="labels">Address Line 1</label></div>
+                            <div class="col-md-12"><label class="labels">Address Line 2</label></div>
+                            <div class="col-md-12"><label class="labels">Postcode</label></div>
+                            <div class="col-md-12"><label class="labels">State</label></div>
+                            <div class="col-md-12"><label class="labels">Area</label></div>
+                            <div class="col-md-12"><label class="labels">Email ID</label></div>
+                            <div class="col-md-12"><label class="labels">Education</label></div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </body>
+    </html>
     );
 };
 
