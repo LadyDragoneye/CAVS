73c71e0c3d77f1d4c30b27ec81843244183e9132
KaylaFredenburg <krfredenburg@gmail.com>
2024-05-02 12:57:44 -0500

    forlexipt2thesaga

--- frontend/src/pages/account/account.js
+++ frontend/src/pages/account/account.js
@@ -5,38 +5,31 @@ import "./account.css";
 
 const Account = () => {
     return (
-    <div class="container rounded bg-white mt-5 mb-5">
-    <div class="row">
-        <div class="col-md-3 border-right">
-            <div class="d-flex flex-column align-items-center text-center p-3 py-5"></div>
-        <div class="col-md-5 border-right">
-            <div class="p-3 py-5">
-                <div class="d-flex justify-content-between align-items-center mb-3">
-                    <h4 class="text-right">Profile</h4>
-                </div>
-                <div class="row mt-2">
-                    <div class="col-md-6"><label class="labels">Name</label></div>
-                </div>
-                <div class="row mt-3">
-                    <div class="col-md-12"><label class="labels">Mobile Number</label></div>
-                    <div class="col-md-12"><label class="labels">Address Line 1</label></div>
-                    <div class="col-md-12"><label class="labels">Address Line 2</label></div>
-                    <div class="col-md-12"><label class="labels">Postcode</label></div>
-                    <div class="col-md-12"><label class="labels">State</label></div>
-                    <div class="col-md-12"><label class="labels">Area</label></div>
-                    <div class="col-md-12"><label class="labels">Email ID</label></div>
-                    <div class="col-md-12"><label class="labels">Education</label></div>
-                </div>
-                <div class="row mt-3">
-                    <div class="col-md-6"><label class="labels">Country</label></div>
-                    <div class="col-md-6"><label class="labels">State/Region</label></div>
+        <div className="container rounded bg-white mt-5 mb-5">
+            <div className="row justify-content-center"> {/* Center the content horizontally */}
+                <div className="col-md-9 border-right">
+                    <div className="p-3 py-5">
+                        <div className="d-flex justify-content-between align-items-center mb-3">
+                            <h4 className="text-right">Profile</h4>
+                        </div>
+                        <div className="row mt-2">
+                            <div className="col-md-6"><label className="labels">Name</label></div>
+                        </div>
+                        <div className="row mt-3">
+                            <div className="col-md-12"><label className="labels">Mobile Number</label></div>
+                            <div className="col-md-12"><label className="labels">Address Line 1</label></div>
+                            <div className="col-md-12"><label className="labels">Address Line 2</label></div>
+                            <div className="col-md-12"><label className="labels">Postcode</label></div>
+                            <div className="col-md-12"><label className="labels">State</label></div>
+                            <div className="col-md-12"><label className="labels">Area</label></div>
+                            <div className="col-md-12"><label className="labels">Email ID</label></div>
+                            <div className="col-md-12"><label className="labels">Education</label></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
-</div>
     );
 };
- 
+
 export default Account;
