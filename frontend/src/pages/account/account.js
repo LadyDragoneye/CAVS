4b799070b49799f11147d5e5347946f1f7592caf
KaylaFredenburg <krfredenburg@gmail.com>
2024-05-02 20:48:14 -0500

    updated home page + account page changes
    :anger: >:(

--- frontend/src/pages/account/account.js
+++ frontend/src/pages/account/account.js
@@ -9,25 +9,22 @@ const Account = () => {
     <body class="body-info">
         <div class="container rounded bg-white mt-5 mb-5">
             <div class="row justify-content-center"> 
-                <div class="col-md-9 border-right">
+                <div class="col-md-9 border-right mr-md-6">
                     <div class="p-3 py-5">
                         <div class="d-flex justify-content-between align-items-center mb-3">
                             <h4 class="text-right">Profile</h4>
                         </div>
-                        <div class="row mt-2">
-                            <div class="col-md-6"><label class="labels">Name</label></div>
-                        </div>
                         <div class="row mt-3">
-                            <div class="col-md-12"><label class="labels">Mobile Number</label></div>
-                            <div class="col-md-12"><label class="labels">Address Line 1</label></div>
-                            <div class="col-md-12"><label class="labels">Address Line 2</label></div>
-                            <div class="col-md-12"><label class="labels">Postcode</label></div>
-                            <div class="col-md-12"><label class="labels">State</label></div>
-                            <div class="col-md-12"><label class="labels">Area</label></div>
-                            <div class="col-md-12"><label class="labels">Email ID</label></div>
-                            <div class="col-md-12"><label class="labels">Education</label></div>
+                        <div class="col-md-6"><label class="labels">Username:</label></div>
+                            <div class="col-md-12"><label class="labels">First Name:</label></div>
+                            <div class="col-md-12"><label class="labels">Last Name:</label></div>
+                            <div class="col-md-12"><label class="labels">Role:</label></div>
+                            <div class="col-md-12"><label class="labels">Email:</label></div>
                         </div>
                     </div>
+                </div>
+                <div class="col-md-6 d-flex justify-content-center">
+                    <button type="button" class="btn btn-primary mb-3">Change Password</button>
                 </div>
             </div>
         </div>
