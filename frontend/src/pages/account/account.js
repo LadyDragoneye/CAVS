d571c27c9828ba9b76822d8ea94060efc5d288a8
KaylaFredenburg <krfredenburg@gmail.com>
2024-05-03 01:42:46 -0500

    account.js

--- frontend/src/pages/account/account.js
+++ frontend/src/pages/account/account.js
@@ -9,30 +9,30 @@ const Account = () => {
     <body class="body-info">
         <div class="container rounded bg-white mt-5 mb-5">
             <div class="row justify-content-center"> 
-                <div class="col-md-9 border-right mr-md-6">
+                <div class="col-md-9 border-right mr-md-6 mx-auto text-center">
                     <div class="p-3 py-5">
-                        <div class="d-flex justify-content-between align-items-center mb-3">
-                            <h4 class="text-right">Profile</h4>
+                        <div class="d-flex justify-content-center mb-3">
+                            <h4>Profile</h4>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-md-6 text-md-left"><label class="labels">Username:</label></div>
+                            <div class="col-md-6"><span>*User</span></div>
                         </div>
                         <div className="row mt-3">
-                            <div className="col-md-6"><label className="labels">Username:</label></div>
-                            <div className="col-md-6"><span>*User</span></div>
+                            <div class="col-md-6"><label class="labels">First Name:</label></div>
+                            <div class="col-md-6"><span>*First Name</span></div>
                         </div>
-                        <div className="row mt-3">
-                            <div className="col-md-6"><label className="labels">First Name:</label></div>
-                            <div className="col-md-6"><span>*First Name</span></div>
+                        <div class="row mt-3">
+                            <div class="col-md-6"><label class="labels">Last Name:</label></div>
+                            <div class="col-md-6"><span>*Last Name</span></div>
                         </div>
-                        <div className="row mt-3">
-                            <div className="col-md-6"><label className="labels">Last Name:</label></div>
-                            <div className="col-md-6"><span>*Last Name</span></div>
+                        <div class="row mt-3">
+                            <div class="col-md-6"><label class="labels">Role:</label></div>
+                            <div class="col-md-6"><span>*Role</span></div>
                         </div>
-                        <div className="row mt-3">
-                            <div className="col-md-6"><label className="labels">Role:</label></div>
-                            <div className="col-md-6"><span>*Role</span></div>
-                        </div>
-                        <div className="row mt-3">
-                            <div className="col-md-6"><label className="labels">Email:</label></div>
-                            <div className="col-md-6"><span>*Email</span></div>
+                        <div class="row mt-3">
+                            <div class="col-md-6"><label class="labels">Email:</label></div>
+                            <div class="col-md-6"><span>*Email</span></div>
                         </div>
                     </div>
                 </div>
